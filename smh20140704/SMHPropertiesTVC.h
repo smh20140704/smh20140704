bff9d73f91f8018f034e8a7ea954ee96946a8573
smh20140704 <smh20140704@gmail.com>
2014-07-07 14:48:24 +0100

    UI layout with dummy cells and placeholder images

---
+++ smh20140704/SMHPropertiesTVC.h
@@ -0,0 +1,13 @@
+//
+//  SMHPropertiesTVC.h
+//  smh20140704
+//
+//  Created by Current User on 07/07/2014.
+//  Copyright (c) 2014 smh20140704. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMHPropertiesTVC : UITableViewController
+
+@end
---
+++ smh20140704/SMHPropertyDetailsVC.m
@@ -0,0 +1,49 @@
+//
+//  SMHPropertyDetailsVC.m
+//  smh20140704
+//
+//  Created by Current User on 07/07/2014.
+//  Copyright (c) 2014 smh20140704. All rights reserved.
+//
+
+#import "SMHPropertyDetailsVC.h"
+
+@interface SMHPropertyDetailsVC ()
+
+@end
+
+@implementation SMHPropertyDetailsVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
